a6caa80a73e2b61bb8b6e02496d5b39107c002a6
甄耀东 <zhent@middleton.school.nz>
2023-03-23 11:42:09 +1300

    update

---
+++ LuckyUnicorn-V1.py
@@ -0,0 +1,22 @@
+""""
+Lucky Unicorn - V1
+Title Screen and Variable Set up
+By: Terry Zhen
+"""
+
+
+# Component 1 - Title Screen and variables
+
+def lucky_unicorn():
+    print("********* Welcome to Lucky Unicorn **********")
+
+    unicorn = 5
+    zebra = 0.5
+    horse = 0.5
+    donkey = 0
+
+
+
+
+# Main Routine
+lucky_unicorn()
